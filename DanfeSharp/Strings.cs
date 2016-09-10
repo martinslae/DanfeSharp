67783fb5bbfcbb64acfffc084c18fc53918fc1e8
Francisco João Nicolau Junior <francisco.nicolaujr@gmail.com>
2016-09-10 12:35:35 -0300

    Commit inicial, versionando versão modificada do DanfeSharp (Permite imprimir um XML não autorizado pela SEFAZ).

---
+++ DanfeSharp/Strings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DanfeSharp
+{
+    internal static class Strings
+    {
+        public const String InscricaoEstadual = "Inscrição Estadual";
+        public const String Endereco = "Endereço";
+        public const String Municipio = "Município";
+        public const String UF = "UF";
+        public const String CnpjCpf = "Cnpj / Cpf";
+        public const String Quantidade = "Quantidade";
+        public const String RazaoSocial = "Razão Social";
+    }
+}
